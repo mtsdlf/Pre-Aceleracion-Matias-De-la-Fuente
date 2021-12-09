252c40d42082b2f6aa99d0ac1c6a44213b8f6a99
Matias De la Fuente <mts.dlf@gmail.com>
2021-12-08 23:30:00 -0300

    update variable names
    update variable names

--- Disney/src/main/java/com/mdelafuente/alkemy/disney/mapper/GenreMapper.java
+++ Disney/src/main/java/com/mdelafuente/alkemy/disney/mapper/GenreMapper.java
@@ -11,14 +11,14 @@ import com.mdelafuente.alkemy.disney.entity.GenreEntity;
 @Component
 public class GenreMapper {
 	
-	public GenreEntity movieDTO2Entity(GenreDTO dto) {
+	public GenreEntity genreDTO2Entity(GenreDTO dto) {
 			GenreEntity genreEntity = new GenreEntity();
 			genreEntity.setName(dto.getName());
 			genreEntity.setImagePath(dto.getImagePath());
 			return genreEntity;
 	}
 
-	public GenreDTO movieEntity2DTO(GenreEntity entity) {
+	public GenreDTO genreEntity2DTO(GenreEntity entity) {
 		GenreDTO dto = new GenreDTO();
 		dto.setId(entity.getId());
 		dto.setName(entity.getName());
@@ -26,10 +26,10 @@ public class GenreMapper {
 		return dto;
 	}
 
-	public List<GenreDTO> movieEntityList2DTOList(List<GenreEntity> entities) {
+	public List<GenreDTO> genreEntityList2DTOList(List<GenreEntity> entities) {
 		List<GenreDTO> dtos = new ArrayList<>();
 		for (GenreEntity entity : entities) {
-			dtos.add(this.movieEntity2DTO(entity));
+			dtos.add(this.genreEntity2DTO(entity));
 		}
 		return dtos;
 	}
